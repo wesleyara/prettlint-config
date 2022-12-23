85abfa23df294157b25dd7bbd6c4ca536571ad22
wesleyara <wes1gom@gmail.com>
2022-12-23 10:25:44 -0300

    feat: adding vscode settings creator

---
+++ src/utils/vsCodeSettings.ts
@@ -0,0 +1,29 @@
+import { existsSync, readFileSync, writeFileSync } from "fs";
+
+import { deepClone } from ".";
+import { settings } from "./constants";
+
+const settingsPath = "./.vscode/settings.json";
+
+export const vsCodeSettings = () => {
+  const existingSettings = existsSync(settingsPath);
+
+  if (!existingSettings) {
+    writeFileSync(settingsPath, JSON.stringify(settings));
+  } else {
+    const fileSettings = JSON.parse(readFileSync(settingsPath, "utf8"));
+    const newFileSettings = deepClone(fileSettings);
+
+    for (const key in settings) {
+      if (!newFileSettings[key]) {
+        newFileSettings[key] = settings[key];
+      }
+    }
+
+    if (JSON.stringify(fileSettings) !== JSON.stringify(newFileSettings)) {
+      writeFileSync(settingsPath, JSON.stringify(newFileSettings));
+    }
+  }
+
+  return existingSettings;
+};
