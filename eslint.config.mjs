ba7986d6bee747772a10fab2595f8a06b0ef9bd4
wesleyara <wes1gom@gmail.com>
2025-01-25 02:26:45 -0300

    chore: update lint for eslint flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,61 @@
+import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import importPlugin from "eslint-plugin-import";
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  {
+    languageOptions: {
+      globals: globals.node,
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+  },
+  pluginJs.configs.recommended,
+  eslintPluginPrettier,
+  eslintConfigPrettier,
+  importPlugin.flatConfigs.recommended,
+  importPlugin.flatConfigs.typescript,
+  ...tseslint.configs.recommended,
+  {
+    ignores: ["node_modules", "dist", "build", "coverage", "lib"],
+    rules: {
+      "no-console": "off",
+      "no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+      "prettier/prettier": [
+        "error",
+        {
+          singleQuote: false,
+          tabWidth: 2,
+          semi: true,
+          trailingComma: "all",
+          printWidth: 100,
+          endOfLine: "auto",
+          arrowParens: "avoid",
+        },
+      ],
+      "import/no-unresolved": "off",
+      "import/order": [
+        "error",
+        {
+          distinctGroup: false,
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+          "newlines-between": "always",
+        },
+      ],
+    },
+  },
+];
